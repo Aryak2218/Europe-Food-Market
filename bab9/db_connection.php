887f635bcc1b330cf8466664818f29e15168d733
Aryak2218 <153180689+Aryak2218@users.noreply.github.com>
2024-06-10 16:37:07 +0700

    Delete bab9 directory

--- bab9/db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'root';
-$password = ''; // dikosongkan karena tidak menggunakan password
-$database = 'europe_food_market';
-
-// Membuat koneksi
-$koneksi = mysqli_connect($servername, $username, $password, $database);
